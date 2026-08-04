89e86049b92dfd0c0d19b6f747852cc9c51898ba
azzhanarif <azzhanarifsat@gmail.com>
2026-08-05 00:22:00 +0500

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -8,9 +8,13 @@ struct Enemy{
     float y;
     bool alive;
     float speed;
+    float hitRate;
+    float Damage;
 
-    Enemy(std::string n,float spawnX,float spawnY,int hth,float spd);
+
+    Enemy(std::string n,float spawnX,float spawnY,int hth,float spd,float rate, float dmg);
     void update();
     static Enemy spawn();
+    void damage();
 
 };
